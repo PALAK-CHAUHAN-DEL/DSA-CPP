1ba44d1f8d476e60626ca90acadade34bb793811
PALAK-CHAUHAN-DEL <palakchauhan16032008@gmail.com>
2025-08-13 13:19:32 +0530

    Add optimised threeSum solution

--- Q5threeSum2.cpp
+++ Q5threeSum2.cpp
@@ -2,10 +2,6 @@
 // a+b+c = 0, then considering -a as the target each time the loop runs we can find possible combinations of b and c.
 #include <iostream>
 #include <vector>
+#include <set>
+#include <algorithm>
 using namespace std;
-
-
-vector<vector<int>> threeSum (vector<int> &nums) {
-    int n = nums.size();
-
-}
---
+++ Q5threeSum3.cpp
@@ -0,0 +1,48 @@
+// Optimised approach to this problem no 15 on leetcode, watch lec 39 for explanation.
+// Just try to understand the sol, tho i sis very well rn...try to think of it yourself next time as this is a classic interview problem.
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+vector <vector<int>> threeSum(vector <int> &nums) {
+     int n = nums.size();
+     vector <vector<int>> ans;
+
+     sort (nums.begin(), nums.end());
+
+     for (int i = 0; i < n; i++) {
+
+        if (i > 0 && nums[i] == nums[i-1]) continue;
+        int j = i + 1, k = n - 1;
+
+        while (j < k) {
+            int sum = nums[i] + nums[j] + nums[k];
+            if (sum < 0) {
+                j++;
+            } else if (sum > 0) {
+                k--;
+            } else {
+                ans.push_back({nums[i], nums[j], nums[k]});
+                j++; k--;
+                while (j < k && nums[j] == nums[j - 1]) j++;
+            }
+        }
+     }
+     return ans;
+}
+
+int main() {
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+    vector<vector<int>> result = threeSum(nums);
+
+    for (const auto& triplet : result) {
+        for (int num : triplet) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
